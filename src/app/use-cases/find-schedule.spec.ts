d12343b3019e45125400b11771ec7243a5d7915f
Arthur Brant <arthurcl.br@hotmail.com>
2023-03-17 17:57:36 -0300

    feat:implement find-schedule use-case

---
+++ src/app/use-cases/find-schedule.spec.ts
@@ -0,0 +1,46 @@
+import { Schedule } from '@app/entities/schedule';
+import { InMemorySchedulesRepository } from '@test/repositories/in-memory-schedules-repository';
+import { FindSchedule } from './find-schedule';
+
+describe('Find schedule use case', () => {
+  it('should find schedule by Id', async () => {
+    const schedulesRepository = new InMemorySchedulesRepository();
+    const findSchedule = new FindSchedule(schedulesRepository);
+
+    await schedulesRepository.create(
+      new Schedule({
+        clientId: 'asfasf',
+        scheduledDate: new Date(2023, 3, 20, 10, 0, 0),
+        service: '15 anos',
+        time: '10:00',
+      }),
+    );
+    await schedulesRepository.create(
+      new Schedule({
+        clientId: 'asfasf',
+        scheduledDate: new Date(2023, 3, 20, 10, 0, 0),
+        service: '15 anos',
+        time: '10:00',
+      }),
+    );
+    await schedulesRepository.create(
+      new Schedule({
+        clientId: 'asfasf',
+        scheduledDate: new Date(2023, 3, 20, 10, 0, 0),
+        service: '15 anos',
+        time: '10:00',
+      }),
+    );
+
+    const { id: scheduleId } = schedulesRepository.schedules[1];
+
+    const { schedule } = await findSchedule.execute({
+      id: scheduleId,
+    });
+
+    expect(schedule.scheduledDate).toMatchObject<Date>(
+      new Date(2023, 3, 20, 10, 0, 0),
+    );
+    expect(schedule.status).toBe('Agendado');
+  });
+});
---
+++ src/app/use-cases/find-schedule.ts
@@ -0,0 +1,29 @@
+import { SchedulesRepository } from '@app/repositories/schedules-repository';
+import { Schedule } from '@app/entities/schedule';
+import { Injectable } from '@nestjs/common';
+import { ScheduleNotFound } from './errors/schedule-not-found';
+
+interface FindScheduleRequest {
+  id: string;
+}
+
+type FindScheduleResponse = {
+  schedule: Schedule;
+};
+
+@Injectable()
+export class FindSchedule {
+  constructor(private schedulesRepository: SchedulesRepository) {}
+
+  public async execute(
+    request: FindScheduleRequest,
+  ): Promise<FindScheduleResponse> {
+    const { id } = request;
+
+    const schedule = await this.schedulesRepository.findById(id);
+
+    if (!schedule) throw new ScheduleNotFound();
+
+    return { schedule };
+  }
+}
