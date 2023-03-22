4b34b1462162186634182b3f9d32904ad32d5bc4
Arthur Brant <arthurcl.br@hotmail.com>
2023-03-22 18:12:32 -0300

    create findallschedules use-case

---
+++ src/app/use-cases/schedules/find-all-schedules.ts
@@ -0,0 +1,5 @@
+import { SchedulesRepository } from '@app/repositories/schedules-repository';
+
+export class FindAllSchedules {
+  constructor(private schedulesRepository: SchedulesRepository) {}
+}
