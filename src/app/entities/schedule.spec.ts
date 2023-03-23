69eb39eeb9ae332be0b276ad9dd7926811a45d54
Arthur Brant <arthurcl.br@hotmail.com>
2023-03-23 18:07:39 -0300

    test if all properties of Schedule are correctly set when created

--- src/app/entities/schedule.spec.ts
+++ src/app/entities/schedule.spec.ts
@@ -2,13 +2,19 @@ import { Schedule } from './schedule';
 
 describe('Schedule', () => {
   it('should create a new schedule', () => {
+    const scheduledDate = new Date(2023, 3, 20, 10, 0, 0);
+
     const schedule = new Schedule({
       service: 'Casal',
-      scheduledDate: new Date(2023, 3, 20, 10, 0, 0),
+      scheduledDate: scheduledDate,
       time: '10:00',
       clientId: 'asdasdas',
     });
 
     expect(schedule.status).toBe('Agendado');
+    expect(schedule.clientId).toBe('asdasdas');
+    expect(schedule.time).toBe('10:00');
+    expect(schedule.scheduledDate).toBe(scheduledDate);
+    expect(schedule.service).toBe('Casal');
   });
 });
